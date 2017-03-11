f2f1bde047ae0ba2e99814fb79f571aeedc005f0
dmzx <info@dmzx-web.net>
2017-03-11 22:41:41 +0100

    Portuguese-Brazil translation

---
+++ language/pt_br/common.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*
+* @package phpBB Extension - Email List
+* @copyright (c) 2015 dmzx - http://www.dmzx-web.net
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'EMAIL_LIST'		=> 'Lista de Email',
+	'EXPORT_LIST'		=> 'Exportar como lista CSV',
+	'USER_COUNT'		=> array(
+		1 => '%d usuário',
+		2 => '%d usuários',
+	),
+	'ALL_GROUPS'		=> 'Todos os grupos',
+	'NO_USERS_IN_GROUP'	=> '<strong>Nenhum usuário encontrado neste grupo</strong>',
+));
